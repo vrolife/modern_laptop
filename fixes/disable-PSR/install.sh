92ab22e4a042fccc42a03fea403a08191540c7a8
pom <pom@vro.life>
2022-08-31 19:22:57 +0800

    update initrd by mkinitcpio -P for arch/manjaro

--- fixes/disable-PSR/install.sh
+++ fixes/disable-PSR/install.sh
@@ -4,6 +4,8 @@ cp disable-psr.conf /etc/modprobe.d/disable-psr.conf
 
 if test "$ID" = "fedora"; then
     dracut --regenerate-all
+elif test "$ID" = "manjaro" -o "$ID_LIKE" = "arch"; then
+    mkinitcpio -P
 else
     update-initramfs -k all -u
 fi
