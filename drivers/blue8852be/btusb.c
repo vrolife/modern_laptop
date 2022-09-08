a6650284b83079bcd8e92b16725035bb24531f2a
pom <pom@vro.life>
2022-09-08 12:34:09 +0800

    [BT/8852BE] blue8852be 1.0.2 for linux 6.x

--- drivers/blue8852be/btusb.c
+++ drivers/blue8852be/btusb.c
@@ -2075,7 +2075,9 @@ static int btusb_setup_csr(struct hci_dev *hdev)
 		 * without these the controller will lock up.
 		 */
 		set_bit(HCI_QUIRK_BROKEN_STORED_LINK_KEY, &hdev->quirks);
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(6, 0, 0))
 		set_bit(HCI_QUIRK_BROKEN_ERR_DATA_REPORTING, &hdev->quirks);
+#endif
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 17, 0))
 		set_bit(HCI_QUIRK_BROKEN_FILTER_CLEAR_ALL, &hdev->quirks);
 		set_bit(HCI_QUIRK_NO_SUSPEND_NOTIFIER, &hdev->quirks);
